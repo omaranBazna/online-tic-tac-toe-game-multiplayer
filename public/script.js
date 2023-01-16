d00e80f966963fa00ac55f72990b12176413960f
omaran bazna <omran4d@gmail.com>
2023-01-16 01:49:44 -0500

    handling multi player game logic

--- public/script.js
+++ public/script.js
@@ -1,3 +1,17 @@
+let player;
+
+fetch("/turn")
+  .then((res) => res.json())
+  .then(({ turn }) => {
+    if (turn) {
+      $(".player").html("X");
+      player = "x";
+    } else {
+      $(".player").html("O");
+      player = "o";
+    }
+  });
+
 $(".cell").click(function () {
   const el = $(this);
   const [x, y] = $(this).attr("data-cor").split("-");
@@ -6,15 +20,15 @@ $(".cell").click(function () {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ x: x, y: y }),
+    body: JSON.stringify({ x, y, player }),
   })
     .then((res) => {
       return res.json();
     })
     .then(({ empty }) => {
+      console.log(empty);
       if (empty) {
-        console.log(el);
-        el.css("background-color", "green");
+        el.css("background-color", player == "x" ? "green" : "red");
       }
     });
 });
--- server.js
+++ server.js
@@ -6,15 +6,27 @@ const state = [
   [0, 0, 0],
   [0, 0, 0],
 ];
+let turn = true;
 app.use(express.json());
 app.use("/", express.static("public"));
+app.get("/turn", (req, res) => {
+  turn = !turn;
+  res.send({ turn });
+});
 app.post("/state", (req, res) => {
-  const { x, y } = req.body;
-  const empty = state[x][y] == 0;
-  if (empty) {
-    state[x][y] = 1;
+  const { x, y, player } = req.body;
+  console.log(player);
+  console.log(!turn);
+  if ((turn && player == "x") || (!turn && player == "o")) {
+    const empty = state[x][y] == 0;
+    if (empty) {
+      state[x][y] = 1;
+    }
+    turn = !turn;
+    res.send({ empty });
+  } else {
+    res.send({ empty: false });
   }
-  res.send({ empty });
 });
 
 app.listen(port, () => {

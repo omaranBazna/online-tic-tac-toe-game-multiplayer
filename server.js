dc81a12ad8bc5643dc054024b6f6173196c153a9
omaran bazna <omran4d@gmail.com>
2023-01-16 01:11:47 -0500

    add script js file

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const state = [
   [0, 0, 0],
   [0, 0, 0],
 ];
+app.use(express.json());
 app.use("/", express.static("public"));
+app.post("/move", (req, res) => {
+  const move = req.body;
+});
 
 app.listen(port, () => {
   console.log("app is listening on port " + port);

51a5c1150e7c6ead71a4fa92db296088c89a75c0
omaran bazna <omran4d@gmail.com>
2023-01-16 01:08:38 -0500

    serving the html pages using express

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const port = 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hi All");
-});
+app.use("/", express.static("public"));
 
 app.listen(port, () => {
   console.log("app is listening on port " + port);
